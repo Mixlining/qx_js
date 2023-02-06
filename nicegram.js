c1a2691f0136b446d74f830f5ef7ec61abc50309
Mixlining <91776353+Mixlining@users.noreply.github.com>
2023-02-06 20:04:53 +0800

    init

---
+++ nicegram.js
@@ -0,0 +1,14 @@
+/******************************
+[rewrite_local]
+
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://github.com/ddgksf2013/dev/raw/main/NicegramProCrack.js
+
+[mitm]
+
+hostname = restore-access.indream.app
+**********************************/
+{
+  "data": {
+    "premiumAccess": true
+  }
+}
